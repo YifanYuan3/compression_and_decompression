a592187cf43b0e39d7255063c6c5a1d2e201a424
Jongse <jspark@gatech.edu>
2017-11-06 13:20:21 -0500

    stall registered

--- pure_HDL_code/Aligner.v
+++ pure_HDL_code/Aligner.v
@@ -15,19 +15,19 @@ module Aligner #(
 	output		[2											:	0]	flags_out, 	// valid, stall, tlast_out
 	output		[DATA_OUT_WIDTH / 8 - 1 : 0]	tkeep
 );
-	wire																				valid, valid_, stall, tlast_in, tlast_in_, flag_compression, is_header, tlast_out, tlast_out_, tlast_out__; 		
-	wire																				prev_valid_out, prev_flag_compression, prev_is_header;
-	wire 			[LEN_WIDTH  								: 0]  in_data_len;
-	wire 			[LEN_WIDTH  						+ 1	: 0]  merged_len;
-	wire 			[LEN_WIDTH  								: 0] 	remained_len_in;
-	wire			[LEN_WIDTH  				    		: 0] 	remained_len_out;
-	wire			[LEN_WIDTH  				    		: 0] 	prev_remained_len_out;
-	wire			[DATA_OUT_WIDTH * 2 + 8 - 1 : 0]	shifted_data_in;
-	wire			[DATA_OUT_WIDTH * 2 + 8 - 1 : 0]	remained_data;
-	wire			[DATA_OUT_WIDTH * 2 + 8 - 1 : 0]	new_out;
-	wire			[DATA_OUT_WIDTH * 2 + 8 - 1 : 0]	old_out;
-	wire 			[DATA_IN_WIDTH 					- 1	: 0] 	prev_data_in;
-	wire			[DATA_OUT_WIDTH / 8 		- 1 : 0]	tkeep_;
+	wire																					valid, valid_, stall, stall_, tlast_in, tlast_in_, flag_compression, is_header, tlast_out, tlast_out_, tlast_out__; 		
+	wire																					prev_valid_out, prev_flag_compression, prev_is_header;
+	wire 			[LEN_WIDTH  									: 0]  in_data_len;
+	wire 			[LEN_WIDTH  						 + 1	: 0]  merged_len;
+	wire 			[LEN_WIDTH  									: 0] 	remained_len_in;
+	wire			[LEN_WIDTH  				    			: 0] 	remained_len_out;
+	wire			[LEN_WIDTH  				    			: 0] 	prev_remained_len_out;
+	wire			[DATA_OUT_WIDTH * 3 + 16 - 1 	: 0]	shifted_data_in;
+	wire			[DATA_OUT_WIDTH * 3 + 16 - 1 	: 0]	remained_data;
+	wire			[DATA_OUT_WIDTH * 3 + 16 - 1 	: 0]	new_out;
+	wire			[DATA_OUT_WIDTH * 3 + 16 - 1 	: 0]	old_out;
+	wire 			[DATA_IN_WIDTH 					 - 1	: 0] 	prev_data_in;
+	wire			[DATA_OUT_WIDTH / 8 		 - 1 	: 0]	tkeep_;
 	
 	assign valid_align				= flags_in[3];
 	assign tlast_in 					= (valid_align == 1'b1) ? flags_in[2] : 0;
@@ -40,7 +40,7 @@ module Aligner #(
 	assign filled 						= (merged_len >= 256) ? 1'b1 : 1'b0;
 	assign overflow 					= (merged_len > 256) ? 1'b1 : 1'b0;
 	assign valid_							= valid_align & (filled | tlast_out__);
-	assign stall							= merged_len >= 512 ? 1'b1 : 1'b0;
+	assign stall_							= merged_len >= 512 ? 1'b1 : 1'b0;
 
 	assign tlast_in_					= (overflow == 1'b1) & (tlast_in == 1'b1) ? 1'b1 : 1'b0;
 	assign tlast_out__				= (overflow == 1'b0) & (tlast_in == 1'b1) ? 1'b1 : tlast_out_;
@@ -68,7 +68,7 @@ module Aligner #(
 	);
 	
 	Register #(
-		.BIT_WIDTH(DATA_OUT_WIDTH * 2 + 8)
+		.BIT_WIDTH(DATA_OUT_WIDTH * 3 + 16)
 	) rem_out_reg (
 		clk,
 		reset, 
@@ -88,13 +88,13 @@ module Aligner #(
 	);
 	
 	Register #(
-		.BIT_WIDTH(1 + 1 + 32)
+		.BIT_WIDTH(1 + 1 + 32 + 1)
 	) last_reg (
 		clk,
 		reset, 
 		wrt_en, 
-		{valid_,	tlast_out__,	tkeep_},
-		{valid,		tlast_out,		tkeep}
+		{valid_,	tlast_out__,	tkeep_, stall_},
+		{valid,		tlast_out,		tkeep,	stall}
 	);
 
 	assign shifted_data_in 	= prev_data_in << prev_remained_len_out;
